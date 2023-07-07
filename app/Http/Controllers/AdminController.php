db1f11d0ca957b6173b814147c19d71d7d275474
Aslan <jasmina031015@gmail.com>
2023-07-07 09:58:48 +0300

    Updating Admin profile

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -25,5 +26,39 @@ class AdminController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/admin/login');
+    } // End Method
+
+    public  function AdminProfile() {
+
+        $id = Auth::user()->id;
+        $adminData = User::find($id);
+        return view('admin.admin_ptofile_view', compact('adminData'));
+    } // End Method
+
+    public function AdminProfileStore(Request $request) {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if ($request->file('photo')) {
+            $file = $request->file('photo');
+            @unlink(public_path('upload/admin_images/'. $data->photo));
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move('public_path'('upload/admin_images/'), $filename);
+            $data['photo'] = $filename;
+        }
+
+        $data->save();
+
+        $notification = array(
+            'message' => 'Admin Profile Updated Successfully',
+            'alert-type' => 'success',
+        );
+
+        return redirect()->back()->with($notification);
+
     } // End Method
 }

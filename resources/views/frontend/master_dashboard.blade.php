7ac6921bdc5b5d5454ae60f74b91cf0d28475730
Aslan <jasmina031015@gmail.com>
2023-07-11 06:46:55 +0300

    Added Frontend Template Register Page Setup

--- resources/views/frontend/master_dashboard.blade.php
+++ resources/views/frontend/master_dashboard.blade.php
@@ -5,7 +5,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Online Shop</title>
+    <title>Easy Shop Online Store</title>
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />

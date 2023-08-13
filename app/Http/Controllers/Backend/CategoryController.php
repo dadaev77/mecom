a9bd64a9ef75ffa6905b1d9fd2c063de032eae9c
Aslan <jasmina031015@gmail.com>
2023-08-13 04:36:15 +0300

    Added Editing Category Dashboard CRUD Part 2

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Brand;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use Image;
@@ -13,6 +14,35 @@ class CategoryController extends Controller
     public function AllCategory(){
         $categories = Category::latest()->get();
         return view('backend.category.category_all',compact('categories'));
+    }
+    // End AllCategory
+
+    public function AddCategory(){
+       return view('backend.category.category_add');
+
+    }
+    // End AddCategory
+
+    public function StoreCategory(Request $request){
+
+        $image = $request->file('category_image');
+        $name_gen = hexdec(uniqid('', '')).'.'. $image->getClientOriginalExtension();
+        Image::make($image)->resize(120,120)->save('upload/category/'.$name_gen);
+        $save_url = 'upload/category/'.$name_gen;
+
+        Category::insert([
+            'category_name' => $request->category_name,
+            'category_slug' => strtolower(str_replace(' ', '-',$request->category_name)),
+            'category_image' => $save_url,
+        ]);
+
+        $notification = array(
+            'message' => 'Category Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->route('all.category')->with($notification);
+
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -103,11 +103,12 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::controller(CategoryController::class)->group(function () {
 
         Route::get('/all/category', 'AllCategory')->name('all.category');
-        Route::get('/add/brand', 'AddBrand')->name('add.brand');
-        Route::post('/brand/store', 'StoreBrand')->name('store.brand');
+        Route::get('/add/category', 'AddCategory')->name('add.category');
+        Route::post('/store/category', 'StoreCategory')->name('store.category');
         Route::get('/edit/brand/{id}', 'EditBrand')->name('edit.brand');
         Route::post('/update/brand', 'UpdateBrand')->name('update.brand');
         Route::get('/delete/brand/{id}', 'DeleteBrand')->name('delete.brand');
+
     });
 
 });

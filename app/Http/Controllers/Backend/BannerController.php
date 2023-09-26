a14a9e809691f86d1bc2852e16ecc28f9baced18
Aslan <jasmina031015@gmail.com>
2023-09-26 15:01:23 +0300

    [29] 002 Backend Banner CRUD Setup Part 2(loading banner image )

--- app/Http/Controllers/Backend/BannerController.php
+++ app/Http/Controllers/Backend/BannerController.php
@@ -15,5 +15,34 @@ class BannerController extends Controller
     public function AllBanner() {
         $banner = Banner::latest()->get();
         return view('backend.banner.banner_all', compact('banner'));
+    } // End AllBanner
+
+
+    public function AddBanner() {
+        return view('backend.banner.banner_add');
+    } // End AddBanner
+
+
+
+    public function StoreBanner(Request $request){
+
+        $image = $request->file('banner_image');
+        $name_gen = hexdec(uniqid('', '')).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(768,450)->save('upload/banner/'.$name_gen);
+        $save_url = 'upload/banner/'.$name_gen;
+
+        Banner::insert([
+            'banner_title' => $request->banner_title,
+            'banner_url' => $request->banner_url,
+            'banner_image' => $save_url,
+        ]);
+
+        $notification = array(
+            'message' => 'Banner Inserted Successfully',
+            'alert-type' => 'info'
+        );
+
+        return redirect()->route('all.banner')->with($notification);
+
     }
 }

7aa9a88ce71e0ee80cd6b20902beb8f5a290c7dd
Aslan <jasmina031015@gmail.com>
2023-10-02 00:42:44 +0300

    [30] 006 Frontend Dynamic New Product Display Part 1

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -1,4 +1,8 @@
 <!-- Header  -->
+@php
+    $products = App\Models\Product::where('status',1)->orderBy('id','ASC')->limit(10)->get();
+@endphp
+
 <header class="header-area header-style-1 header-height-2">
     <div class="mobile-promotion">
         <span>Grand opening, <strong>up to 15%</strong> off all items. Only <strong>3 days</strong> left</span>

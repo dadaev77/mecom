45cddd381f2b291f0007f1bbd79d10ee5e106958
Aslan <jasmina031015@gmail.com>
2023-10-09 07:31:54 +0300

    [31] 006 Product Details Show Part 6(added vendor images logo, address, phone, short info for product_details.blade.php template)

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -26,6 +26,7 @@ class IndexController extends Controller
         $multiImage = MultiImg::where('product_id', $id)->get();
 
 
+
         return view('frontend.product.product_details',compact('product','product_color','product_size', 'multiImage'));
 
     } // End ProductDetails

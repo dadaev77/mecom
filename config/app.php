85ecd322c6db35f7e7a587dcb20e68dd02f2d167
Aslan <jasmina031015@gmail.com>
2023-11-29 07:47:36 +0300

    [35] 001 Product View Modal With Ajax Part 1(added column: Size, Color with choice and Brand, Product Code, Category, Stock )

--- config/app.php
+++ config/app.php
@@ -212,6 +212,8 @@ return [
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
         'Image' => Intervention\Image\Facades\Image::class,
+
+        'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
     ])->toArray(),
 
 ];
